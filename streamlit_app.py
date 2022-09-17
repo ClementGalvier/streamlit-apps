4bbedbee7bfe3474f16da250594cc7f6e0c5f6dc
ClementGalvierGitpersonal <105963254+ClementGalvierGitHub@users.noreply.github.com>
2022-09-17 23:04:43 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,20 @@ import requests
 import json
 
 # Title of the page
-st.title("🎓 Graduate School Admissions")
+st.title("🎓 DunkinOrStarbucks")
 
 # Get user inputs
-gre = st.number_input("📚 GRE Score:", min_value=0, max_value=800, help="GRE score in the range 0 to 800") # int max value to allow only int inputs
-gpa = st.number_input("✍️ GPA Score:", min_value=0.0, max_value=5.0, help="GPA in the range 0 to 5") # float max value to allow decimal inputs
+comment  =st.text_input("Comment", help ="paste your comment here to check whether it belongs to Dunkin or Starbucks subreddit" )
+
 
 # Display the inputs
-user_input = {"gre":gre,"gpa":gpa}
+user_input = {"comment":comment}
 st.write("User input:")
 st.write(user_input)
+
 # Code to post the user inputs to the API and get the predictions
 # Paste the URL to your GCP Cloud Run API here!
-api_url = 'https://grad-school-admission-a4rmk57awq-as.a.run.app'
+api_url = 'https://dunkinorstarbucks-jqjv2zme2q-as.a.run.app'
 api_route = '/predict'
 
 response = requests.post(f'{api_url}{api_route}', json=json.dumps(user_input)) # json.dumps() converts dict to JSON
